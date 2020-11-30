323ba1cbe7787cb23b8f4a480a8671452c1f78e8
JoelSanRam <troya48_pumas@hotmail.com>
2020-11-30 16:41:06 -0600

    Update detalleproject.blade.php

--- resources/views/cliente/detalleproject.blade.php
+++ resources/views/cliente/detalleproject.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <title>Sofía Coná | Detalle de obra</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Archi is best selling interior design website template with responsive stunning design">
+    <meta name="description" content="Sofía Coná">
     <meta name="keywords" content="architecture,building,business,bootstrap,creative,exterior design,furniture design,gallery,garden design,house,interior design,landscape design,multipurpose,onepage,portfolio,studio">
     <meta name="author" content="">
 
