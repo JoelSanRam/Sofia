b298714a236ffb84ac64909ea53c4315631c0ddf
urielspv <urielperez9781@gmail.com>
2021-02-24 17:32:34 -0600

    meta tag

--- resources/views/MasterPage/cliente.blade.php
+++ resources/views/MasterPage/cliente.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title>Sofía Coná</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Archi is best selling interior design website template with responsive stunning design">
+    <meta name="description" content="Hola, mi nombre es Sofía, me encanta pintar y escribir, creo que todos tenemos mucho adentro y necesitamos algo para no explotar. Lo mío es pintar, ¿y lo tuyo?">
     <meta name="keywords" content="architecture,building,business,bootstrap,creative,exterior design,furniture design,gallery,garden design,house,interior design,landscape design,multipurpose,onepage,portfolio,studio">
     <meta name="author" content="">
 
